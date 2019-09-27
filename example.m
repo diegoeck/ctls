724ee789392f29ad26b5ca081a43d167063ac65b
Diego Eckhard <diegoeck@ufrgs.br>
2019-09-27 15:37:26 -0300

    Fix example

--- example.m
+++ example.m
@@ -1,4 +1,5 @@
 clc
+clear all
 
 N=1000
 M=100
@@ -19,14 +20,15 @@ v1=randn(N,1);
 
 xr=x+0.2*v1;
 yr=y+0.3*v1;
-zr=z+0.4*v1;
+zr=z+4*v1;
 
 Phi=[xr yr];
 
 kls=Phi\zr; 
+Z=[Phi zr];
 ktls=inv(Phi'*Phi - min(svd(Z'*Z))*eye(2))*Phi'*zr;
 
-F=[0.2*ones(N,1) 0.3*ones(N,1) 0.4*ones(N,1)];
+F=[0.2*ones(N,1) 0.3*ones(N,1) 4*ones(N,1)];
 kctls=ctls(Phi,zr,F);
 
 Kls   =[Kls   kls];
@@ -50,10 +52,10 @@ figure(2)
 plot(Kls(1,:),Kls(2,:),'o')
 hold on
 plot(Ktls(1,:),Ktls(2,:),'o')
-plot(K1(1,:),K1(2,:),'o')
-plot(kmls(1),kmls(2),'o')
-plot(kmtls(1),kmtls(2),'o')
-plot(km1(1),km1(2),'o')
+plot(Kctls(1,:),Kctls(2,:),'o')
+plot(kls_m(1),kls_m(2),'o')
+plot(ktls_m(1),ktls_m(2),'o')
+plot(kctls_m(1),kctls_m(2),'o')
 plot(2,3,'o')
 hold off
 legend('LS','TLS','CTLS','mLS','mTLS','mCTLS','correct','Location','southeast')
